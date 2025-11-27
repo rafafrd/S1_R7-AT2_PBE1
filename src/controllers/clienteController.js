86dccd986da804894f651be36680d7f936c5f079
rafafrd <bolekao03@gmail.com>
2025-11-27 10:27:01 -0300

    feat: select working

--- src/controllers/clienteController.js
+++ src/controllers/clienteController.js
@@ -1,6 +1,29 @@
 const { clienteModel } = require("../models/clienteModel");
 
-const controllerFunc = {
+const clienteController = {
+  /**
+   * Busca e retorna todos os clientes cadastrados.
+   * Rota GET /clientes
+   * @async
+   * @function selecionaTodos
+   * @param {Request} req Objeto da requisição HTTP
+   * @param {Response} res Objeto da resposta HTTP
+   * @returns {Promise<Response>} Retorna um JSON com a lista de clientes ou uma mensagem.
+   */
+  selecionaTodos: async (req, res) => {
+    try {
+      const resultado = await clienteModel.selectAll();
+      if (resultado.length === 0) {
+        return res
+          .status(200)
+          .json({ message: "A consulta não retornou resultados" });
+      }
+      res.status(200).json({ data: resultado });
+    } catch (error) {
+      console.error(`Erro ao executar: ${error}`);
+      res.status(500).json({ message: "Ocorreu um erro no servidor" });
+    }
+  },
   /**
    *
    * @param {*} req
@@ -41,4 +64,4 @@ const controllerFunc = {
   },
 };
 
-module.exports = { controllerFunc };
+module.exports = { clienteController };
