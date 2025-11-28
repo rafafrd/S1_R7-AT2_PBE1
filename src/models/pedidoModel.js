1d2ca44a59d402e63cac5a9c7326d298bfb25e2f
rafafrd <bolekao03@gmail.com>
2025-11-28 08:23:39 -0300

    feat: insert pedido

--- src/models/pedidoModel.js
+++ src/models/pedidoModel.js
@@ -3,9 +3,25 @@ const { pool } = require("../config/db");
 const pedidoModel = {
   // Busca de todos os clientes;
   selectAll: async () => {
-    const sql = "SELECT * FROM clientes;";
+    const sql = "SELECT * FROM pedido;";
     const [rows] = await pool.query(sql);
     return rows;
+  },
+  insertPedido: async (pIdCliente, pPesoCarga) => {
+    const connection = await pool.getConnection();
+    try {
+      await connection.beginTransaction();
+      // insert 1 - pedido
+      const sqlPedido =
+        "INSERT INTO pedidos (id_cliente, peso_carga) VALUES (?,?);";
+      const valuesPedido = [pIdCliente, pPesoCarga];
+      const [rowsPedido] = await connection.query(sqlPedido, valuesPedido);
+      connection.commit();
+      return { rowsPedido };
+    } catch (error) {
+      connection.rollback();
+      throw error;
+    }
   },
 };
 
