184e30073c23f0348dc9b71537dfdf8de11d0a16
rafafrd <bolekao03@gmail.com>
2025-12-05 09:52:04 -0300

    env

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,13 @@
-require("dotenv").config();
+require("dotenv").config(); // Carrega as variáveis do arquivo .env
 
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: "127.0.0.1",
-  user: "root",
-  password: "1234",
-  database: "rapido_seguro_db",
-  port: "3306",
+  host: process.env.DB_HOST || "127.0.0.1",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "1234",
+  database: process.env.DB_NAME || "rapido_seguro_db",
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true, // Aguardar conexões livres
   connectionLimit: 10, // limita o número de conexões simultâneas
   queueLimit: 0, // 0 = sem limite para fila
@@ -16,7 +16,7 @@ const pool = mysql.createPool({
 (async () => {
   try {
     const connection = await pool.getConnection();
-    console.log("Conexão com o My SQL foi estabelecida com sucesso");
+    console.log("Conexão com o MySQL foi estabelecida com sucesso");
     connection.release();
   } catch (error) {
     console.error(`Erro ao conectar com o banco de dados: ${error}`);
